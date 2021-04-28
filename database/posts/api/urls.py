df93341e74288ed754d0c8701590e912416e10cc
it21871-dev <it21871@hua.gr>
2021-04-28 21:37:21 +0300

    Movies Detail Api created

--- database/posts/api/urls.py
+++ database/posts/api/urls.py
@@ -2,8 +2,10 @@ from django.conf.urls import url
 from django.urls import path, include
 from .views import (
     MoviesListApiView,
+    MoviesDetailApiView
 )
 
 urlpatterns = [
     path('', MoviesListApiView.as_view()),
+    path('<int:movie_id>/', MoviesDetailApiView.as_view()),
 ]
--- database/posts/api/views.py
+++ database/posts/api/views.py
@@ -38,3 +38,74 @@ class MoviesListApiView(APIView):
             return Response(serializer.data, status=status.HTTP_201_CREATED)
 
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+class MoviesDetailApiView(APIView):
+
+    def get_object(self, movie_id):
+        '''
+        Helper method to get the object with given movie_id
+        '''
+        try:
+            return movie.objects.get(id=movie_id)
+        except movie.DoesNotExist:
+            return None
+
+    # 3. Retrieve
+    def get(self, request, movie_id, *args, **kwargs):
+        '''
+        Retrieves the movie with given movie_id
+        '''
+        movie_instance = self.get_object(movie_id)
+        if not movie_instance:
+            return Response(
+                {"res": "Object with movie id does not exists"},
+                status=status.HTTP_400_BAD_REQUEST
+            )
+
+        serializer = movieSerializer(movie_instance)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+    # 4. Update
+    def put(self, request, movie_id, *args, **kwargs):
+        '''
+        Updates the movie item with given movie_id if exists
+        '''
+        movie_instance = self.get_object(movie_id)
+        if not movie_instance:
+            return Response(
+                {"res": "Object with movie id does not exists"},
+                status=status.HTTP_400_BAD_REQUEST
+            )
+        data = {
+            'title': request.data.get('title'),
+            'director': request.data.get('director'),
+            'actors': request.data.get('actors'),
+            'genre': request.data.get('genre'),
+            'release_date': request.data.get('release_date'),
+            'rating': request.data.get("rating")
+        }
+        serializer = movieSerializer(instance = movie_instance, data=data, partial = True)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    # 5. Delete
+    def delete(self, request, movie_id, *args, **kwargs):
+        '''
+        Deletes the movie item with given movie_id if exists
+        '''
+        movie_instance = self.get_object(movie_id)
+        if not movie_instance:
+            return Response(
+                {"res": "Object with movie id does not exists"}, 
+                status=status.HTTP_400_BAD_REQUEST
+                                                                                                                        )
+        movie_instance.delete()
+        return Response(
+            {"res": "Object deleted!"},
+            status=status.HTTP_200_OK
+        )
+
+## More API Views soon... ##
+
