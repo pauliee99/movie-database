0dc7ce4a63db6919dc6f5b2c436f63d2f5c98ed8
it21871-dev <it21871@hua.gr>
2021-05-08 19:05:26 +0300

    customize admin page

--- database/posts/admin.py
+++ database/posts/admin.py
@@ -3,6 +3,13 @@ from django.contrib import admin
 # Register your models here.
 from .models import movie, user, viewer, manager
 
-admin.site.register(movie)
+class MovieAdmin(admin.ModelAdmin):
+    fieldsets = [
+        ('Movie title and rating', {'fields': ['title', 'rating']}),
+        ('Date information', {'fields': ['release_date']}),
+    ]
+
+admin.site.register(movie, MovieAdmin)
+
 admin.site.register(viewer)
 admin.site.register(manager)
