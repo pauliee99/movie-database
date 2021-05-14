50be9eb3d2a6295a29400fd56950c97f22d798f4
it21871-dev <it21871@hua.gr>
2021-05-14 15:22:07 +0300

    working on auth api

--- database/posts/api/serializers.py
+++ database/posts/api/serializers.py
@@ -6,33 +6,14 @@ class MovieSerializer(serializers.ModelSerializer):
     class Meta:
         model = models.Movie
         fields = ["id", "title", "director", "actors", "genre", "release_date", "rating"]
+
 """
 class UserSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = User
         fields = ['url', 'username', 'email', 'is_staff']
 """
-'''
-class userSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = models.user
-        fields = ["id", "username", "email", "password", "date_registered"]
-'''
-"""
-Here, maybe its wrong to use inheritance through serializer classes
-If something goes wrong, do exactly the same as the user serializer for the next 2 classes
-"""
-"""
-class viewerSerializer(UserSerializer):
-    class Meta:
-        model = models.viewer
-        fields = ["movies_viewed", "movies_rated", "movies_loved"]
 
-class managerSerializer(UserSerializer):
-    class Meta:
-        model = models.manager
-        fields = ["movies_added"]
-"""
 class EmailSerializer(serializers.ModelSerializer):
     class Meta:
         model = models.Email
--- database/posts/api/urls.py
+++ database/posts/api/urls.py
@@ -4,9 +4,6 @@ from rest_framework import routers
 from .views import (
     MoviesListApiView,
     MoviesDetailApiView,
-    #UserViewSet,
-    #UsersListApiView,
-    #UsersDetailApiView,
     EmailListApiView,
     EmailDetailApiView
 )
@@ -18,8 +15,6 @@ urlpatterns = [
     path('', MoviesListApiView.as_view()),
     path('<int:movie_id>/', MoviesDetailApiView.as_view()),
     #path('users/', include(router.urls)),
-    #path('users/', UsersListApiView.as_view()),
-    #path('users/<int:user_id>/', UsersDetailApiView.as_view()),
     path('mails/', EmailListApiView.as_view()),
     path('mails/<int:email_id>/', EmailDetailApiView.as_view()),
 ]
